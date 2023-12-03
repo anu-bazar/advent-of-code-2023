3b2c01b5299faf5d18225cb99f4767f76c308c53
anu-bazar <anubazarragcaa@gmail.com>
2023-12-03 22:25:18 +0100

    line counter

--- day-1/main.py
+++ day-1/main.py
@@ -1,7 +1,33 @@
 #print("Hello World")
+import re
 # Using readlines()
-file1 = open('input.txt', 'r')
+file1 = open('day-1\input.txt', 'r')
 Lines = file1.readlines()
-print(Lines)
+#print(Lines)
+#create empty list
+elf_numbers=[]
+for line in Lines:
+    numbers = re.findall(r'\d+', line) #'/d' - digit from 0 to 9 AND '+' - one or more occurences 
+    for number in numbers:
+        elf_numbers.append(int(number))
+#print(elf_numbers)
+
+
+
+
+
+
+"""if its a letter, then do not extract
+if its a number then do extract and store it into a new line
+	if the number is higher than 2
+	take the 1st number and last number
+	if the number is 2
+	take the 1st number and last number
+	if the number is one
+	the number should be doubled """
+
+
+		
+
 #close your reader
 file1.close()
